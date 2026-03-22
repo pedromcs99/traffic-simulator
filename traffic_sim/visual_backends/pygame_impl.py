8c4bbe7119373a93bf1086c1df79ef971b2f7772
Pedro Sousa <pedromcs99@gmail.com>
2026-03-22 21:14:03 +0000

    started working on semaphores look

--- traffic_sim/visual_backends/pygame_impl.py
+++ traffic_sim/visual_backends/pygame_impl.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 from dataclasses import dataclass
+import math
 
 import pygame
 
@@ -222,26 +223,26 @@ class _PygameViewBase:
             return (180, 0, 0)
 
         circle_radius = 18
-        half_road_plus_diameter = INTERSECTION_HALF_PX + circle_radius * 2
+        half_road_plus_diameter = INTERSECTION_HALF_PX + circle_radius
         # vertical lane
         pygame.draw.circle(
             self.screen,
             color_for(ns_state),
-            (center_x - half_road_plus_diameter, center_y - half_road_plus_diameter),
-            circle_radius,0,False,True,True,False
+            (center_x - half_road_plus_diameter, center_y - (half_road_plus_diameter -circle_radius)),
+            circle_radius,0,True,True,False,False
         )
         pygame.draw.circle(
             self.screen,
             color_for(ns_state),
-            (center_x + half_road_plus_diameter, center_y + half_road_plus_diameter),
-            circle_radius,
+            (center_x + half_road_plus_diameter, center_y + (half_road_plus_diameter - circle_radius)),
+            circle_radius,0,False,False,True,True
         )
         # horizontal lane
         pygame.draw.circle(
             self.screen,
             color_for(ew_state),
             (center_x - half_road_plus_diameter, center_y + half_road_plus_diameter),
-            circle_radius,
+            circle_radius,0,True,True,False,False
         )
         pygame.draw.circle(
             self.screen,
